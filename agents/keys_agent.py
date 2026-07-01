b53b1aa3366c284e7810b27e968f5be95c82f808
jonny elfasi <johnosaki94@gmail.com>
2026-07-02 00:27:45 +0700

    Add PAYPAL_CLIENT_ID and PAYPAL_CLIENT_SECRET to keys_agent

--- agents/keys_agent.py
+++ agents/keys_agent.py
@@ -4,6 +4,8 @@ KEYS = {
     "ANTHROPIC_API_KEY": os.environ.get("ANTHROPIC_API_KEY", ""),
     "SUPABASE_URL": os.environ.get("SUPABASE_URL", ""),
     "SUPABASE_SERVICE_KEY": os.environ.get("SUPABASE_SERVICE_KEY", ""),
+    "PAYPAL_CLIENT_ID": os.environ.get("PAYPAL_CLIENT_ID", ""),
+    "PAYPAL_CLIENT_SECRET": os.environ.get("PAYPAL_CLIENT_SECRET", ""),
 }
 
 def get_key(name):
